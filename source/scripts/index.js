7b43159313182703e67299a2071ca722226a87f7
Skripkina-Lyudmila <msk.revizor@gmail.com>
2025-12-16 09:23:22 +0300

    Дорабатывает слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,5 @@
 /* в этот файл добавляет скрипты*/
+const container = document.querySelector('.compare-slider');
+document.querySelector('.compare-slider__range-handle').addEventListener('input', (e) => {
+  container.style.setProperty('--js-position', `${e.target.value}%`);
+});
