1ed7813bc24c210cb5cfb3e222c77d07a1f5a12d
Devexlein <68198080+Devexlein@users.noreply.github.com>
2025-12-19 15:40:28 +0000

    Deploying to gh-pages from @ Devexlein/1750975-cat-energy-2@e4112b8c76acd76bf581f0f3fb2e8a4eb994918b 🚀

--- 7/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var t=document.querySelector(".compare-slider");document.querySelector(".compare-slider__range-handle").addEventListener("input",e=>{t.style.setProperty("--js-position",`${e.target.value}%`)});
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var t=document.querySelector(".compare-slider");document.querySelector(".compare-slider__range-handle").addEventListener("input",e=>{t.style.setProperty("--js-position",`${e.target.value}%`)});
